d8473c0559df375ac15916d0bc3065c830a74414
ryllevisto2020 <ryllevisto13@gmail.com>
2025-11-18 18:17:01 +0800

    server

--- index.js
+++ index.js
@@ -19,74 +19,85 @@ app.use(cors({
 const conn = async () =>{
     return await mysql.createConnection({
         host:"localhost",
-        user:"psych",
-        password:"1234",
+        user:"root",
+        password:"",
         database:"psychpath"
     });
 }
 
 async function updateTally(id,survey){
-    save_rate = (await conn()).query(`UPDATE tbl_pre_survey SET ${survey} = ${survey} + 1 WHERE question_id = ${id}`)
+    save_rate = (await conn()).query(`UPDATE tbl_pre_survey SET ${survey} = ${survey} + 1 WHERE question_id = ${id}`).then((c)=>{
+        console.log("end")
+    })
 }
 
 async function updateTallyPost(id,survey){
-    save_rate = (await conn()).query(`UPDATE tbl_post_survey SET ${survey} = ${survey} + 1 WHERE question_id = ${id}`)
+    save_rate = (await conn()).query(`UPDATE tbl_post_survey SET ${survey} = ${survey} + 1 WHERE question_id = ${id}`).then((c)=>{
+        console.log("end")
+    })
 }
 
 app.post("/api/registration",async (req,res)=>{
     addAccount = (await conn()).query(
         `INSERT INTO tbl_account (id,acc_username,acc_password,acc_name,acc_age,acc_section,acc_gender) `+
-        `VALUES(null,'${req.body.username}','${req.body.password}','${req.body.name}','${req.body.age}','${req.body.section}','${req.body.gender}')`);
-    
-    (await conn()).destroy();
-
-    return res.json({code:200,message:"Accepted"})
+        `VALUES(null,'${req.body.username}','${req.body.password}','${req.body.name}','${req.body.age}','${req.body.section}','${req.body.gender}')`).then(async (a)=>{
+            await conn.end
+            return res.json({code:200,message:"Accepted"})  
+        })
 })
 
 app.post("/api/login",async (req,res)=>{
     loginAccount = (await conn()).query(`SELECT * FROM tbl_account WHERE 
         acc_username LIKE '${req.body.username}' AND 
-        acc_password LIKE '${req.body.password}'`)
-    account = await loginAccount
-
-    if(account.length > 0 ){
-        (await conn()).destroy();
-        return res.json({code:200,message:"Accepted!",id:account[0].id});
-    }else{
-        (await conn()).destroy();
-        return res.json({code:500,message:"Rejected!"});
-    }
+        acc_password LIKE '${req.body.password}'`).then(async (c)=>{
+            if(c.length > 0 ){
+                await conn.end
+                return res.json({code:200,message:"Accepted!",id:c[0].id});
+            }else{
+                await conn.end
+                return res.json({code:500,message:"Rejected!"});
+            }
+        })
 })
 
 app.post("/api/level/one/save",async (req,res)=>{
-    find = (await conn()).query(`SELECT * FROM tbl_lvl_one WHERE acc_id = ${req.body.id}`)
-    length = await find
-    if(length.length > 0){
-        update= (await conn()).query(`
-            UPDATE tbl_lvl_one SET res_clinical = '${Math.round((req.body.count.A/18) * 50)}',
-            res_industrial = '${Math.round((req.body.count.B/18) * 50)}',
-            res_educational = '${Math.round((req.body.count.C/18) * 50)}' WHERE acc_id = '${req.body.id}'
-            `);
-    }else{
-        save = (await conn()).query(`INSERT INTO tbl_lvl_one (id,acc_id,res_clinical,res_industrial,res_educational	) 
-        VALUES(null,'${req.body.id}','${Math.round((req.body.count.A/18) * 50)}','${Math.round((req.body.count.B/18) * 50)}','${Math.round((req.body.count.C/18) * 50)}')`);
-    }
-    (await conn()).destroy();
+    find = (await conn()).query(`SELECT * FROM tbl_lvl_one WHERE acc_id = ${req.body.id}`).then(async (c)=>{
+        if(c.length > 0){
+            update= (await conn()).query(`
+                UPDATE tbl_lvl_one SET res_clinical = '${Math.round((req.body.count.A/18) * 50)}',
+                res_industrial = '${Math.round((req.body.count.B/18) * 50)}',
+                res_educational = '${Math.round((req.body.count.C/18) * 50)}' WHERE acc_id = '${req.body.id}'
+                `).then((c)=>{
+                    console.log("end")
+                })
+            }else{
+                save = (await conn()).query(`INSERT INTO tbl_lvl_one (id,acc_id,res_clinical,res_industrial,res_educational	) 
+                    VALUES(null,'${req.body.id}','${Math.round((req.body.count.A/18) * 50)}','${Math.round((req.body.count.B/18) * 50)}','${Math.round((req.body.count.C/18) * 50)}')`).then((c)=>{
+                        console.log("end")
+                    })
+                }
+            })
+    await conn.end
     return res.json({code:200,message:"Accepted!"})
 })
 
 app.post("/api/level/two/save",async(req,res)=>{
-    find = (await conn()).query(`SELECT * FROM tbl_lvl_two WHERE acc_id = ${req.body.id}`)
-    length = await find
-    if(length.length > 0){
-        update = (await conn()).query(`UPDATE tbl_lvl_two SET res_clinical = '${Math.round((req.body.Clinical/5) * 50)}',
-        res_educational = '${Math.round((req.body.Educational/5) * 50)}',
-        res_industrial = '${Math.round((req.body.Industrial/5) * 50)}' WHERE acc_id = ${req.body.id}`)
-    }else{
-        save = (await conn()).query(`INSERT INTO tbl_lvl_two (id,acc_id,res_clinical,res_educational,res_industrial) 
-        VALUES(null,'${req.body.id}','${Math.round((req.body.Clinical/5) * 50)}','${Math.round((req.body.Educational/5) * 50)}','${Math.round((req.body.Industrial/5) * 50)}')`)
-    }
-	(await conn()).destroy();
+    find = (await conn()).query(`SELECT * FROM tbl_lvl_two WHERE acc_id = ${req.body.id}`).then(async (c)=>{
+        if(c.length > 0){
+            update = (await conn()).query(`UPDATE tbl_lvl_two SET res_clinical = '${Math.round((req.body.Clinical/5) * 50)}',
+            res_educational = '${Math.round((req.body.Educational/5) * 50)}',
+            res_industrial = '${Math.round((req.body.Industrial/5) * 50)}' WHERE acc_id = ${req.body.id}`).then((c)=>{
+                console.log("end")
+            })
+        }else{
+            save = (await conn()).query(`INSERT INTO tbl_lvl_two (id,acc_id,res_clinical,res_educational,res_industrial) 
+                VALUES(null,'${req.body.id}','${Math.round((req.body.Clinical/5) * 50)}','${Math.round((req.body.Educational/5) * 50)}','${Math.round((req.body.Industrial/5) * 50)}')`).then((c)=>{
+                    console.log("end")
+                })
+            }
+        })
+
+	await conn.end
     return res.json({code:200,message:"Accepted!"})
 })
 
@@ -99,19 +110,27 @@ app.post("/api/survey/pre",async (req,res)=>{
     for (let index = 0; index < data.length; index++) {
         if(data[index].rate == '1'){
             const survey = 'survey_rate_1';
-            await updateTally(data[index].id,survey)
+            await updateTally(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
         if(data[index].rate == '2'){
             const survey = 'survey_rate_2';
-            await updateTally(data[index].id,survey)
+            await updateTally(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
         if(data[index].rate == '3'){
             const survey = 'survey_rate_3';
-            await updateTally(data[index].id,survey)
+            await updateTally(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
         if(data[index].rate == '4'){
             const survey = 'survey_rate_4';
-            await updateTally(data[index].id,survey)
+            await updateTally(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
     }
 
@@ -127,37 +146,48 @@ app.post("/api/survey/pre",async (req,res)=>{
             percentage_set_C = percentage_set_C + Number.parseInt(data[index].score)
         }
     }
-    find_interest = (await conn()).query(`SELECT * FROM tbl_career_interest WHERE acc_id = ${req.body.id}`)
-    find_attitude = (await conn()).query(`SELECT * FROM tbl_career_attitude WHERE acc_id = ${req.body.id}`)
-    find_skills = (await conn()).query(`SELECT * FROM tbl_career_skills WHERE acc_id = ${req.body.id}`)
-    length_i = await find_interest
-    length_a = await find_attitude
-    length_s = await find_skills
-    if(length_i.length > 0){
-        update = (await conn()).query(`UPDATE tbl_career_interest SET score = '${Math.round(percentage_set_A/4)}' 
-        WHERE acc_id = '${req.body.id}'`)
-    }else{
-        save_interest = (await conn()).query(`INSERT INTO tbl_career_interest (id,acc_id,score) 
-        VALUES(null,'${req.body.id}','${Math.round(percentage_set_A/4)}')`);
-    }
-    
-    if(length_a.length > 0){
-        update = (await conn()).query(`UPDATE tbl_career_attitude SET score = '${Math.round(percentage_set_B/5)}' 
-        WHERE acc_id = '${req.body.id}'`)
-    }else{
-        save_attitude = (await conn()).query(`INSERT INTO tbl_career_attitude (id,acc_id,score) 
-        VALUES(null,'${req.body.id}','${Math.round(percentage_set_B/5)}')`)
-    }
+    find_interest = (await conn()).query(`SELECT * FROM tbl_career_interest WHERE acc_id = ${req.body.id}`).then(async (c)=>{
+        if(c.length > 0){
+            update = (await conn()).query(`UPDATE tbl_career_interest SET score = '${Math.round(percentage_set_A/4)}' 
+            WHERE acc_id = '${req.body.id}'`).then((c)=>{
+                console.log("end")
+            })
+        }else{
+            save_interest = (await conn()).query(`INSERT INTO tbl_career_interest (id,acc_id,score) 
+                VALUES(null,'${req.body.id}','${Math.round(percentage_set_A/4)}')`).then((c)=>{
+                    console.log("end")
+                })
+            }
+        })
 
-    if(length_s.length > 0){
-        update = (await conn()).query(`UPDATE tbl_career_skills SET score = '${Math.round(percentage_set_C/6)}' 
-        WHERE acc_id = '${req.body.id}'`)
-    }else{
-        save_skills = (await conn()).query(`INSERT INTO tbl_career_skills (id,acc_id,score) 
-        VALUES(null,'${req.body.id}','${Math.round(percentage_set_C/6)}')`)
-    }
-   (await conn()).destroy(); 
+    find_attitude = (await conn()).query(`SELECT * FROM tbl_career_attitude WHERE acc_id = ${req.body.id}`).then(async (c)=>{
+        if(c.length > 0){
+            update = (await conn()).query(`UPDATE tbl_career_attitude SET score = '${Math.round(percentage_set_B/5)}' 
+            WHERE acc_id = '${req.body.id}'`).then((c)=>{
+                console.log("end")
+            })
+        }else{
+            save_attitude = (await conn()).query(`INSERT INTO tbl_career_attitude (id,acc_id,score) 
+                VALUES(null,'${req.body.id}','${Math.round(percentage_set_B/5)}')`).then((c)=>{
+                    console.log("end")
+                })
+            }
+        })
 
+    find_skills = (await conn()).query(`SELECT * FROM tbl_career_skills WHERE acc_id = ${req.body.id}`).then(async (c)=>{
+        if(c.length > 0){
+            update = (await conn()).query(`UPDATE tbl_career_skills SET score = '${Math.round(percentage_set_C/6)}' 
+            WHERE acc_id = '${req.body.id}'`).then((c)=>{
+                console.log("end")
+            })
+        }else{
+            save_skills = (await conn()).query(`INSERT INTO tbl_career_skills (id,acc_id,score) 
+                VALUES(null,'${req.body.id}','${Math.round(percentage_set_C/6)}')`).then((c)=>{
+                    console.log("end")
+                })
+            }
+        })
+        await conn.end
     return res.json({A:percentage_set_A/4,B:percentage_set_B/5,C:percentage_set_C/6})
 })
 
@@ -167,53 +197,75 @@ app.post("/api/survey/post",async(req,res)=>{
     for (let index = 0; index < data.length; index++) {
         if(data[index].rate == '1'){
             const survey = 'survey_rate_1';
-            await updateTallyPost(data[index].id,survey)
+            await updateTallyPost(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
         if(data[index].rate == '2'){
             const survey = 'survey_rate_2';
-            await updateTallyPost(data[index].id,survey)
+            await updateTallyPost(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
         if(data[index].rate == '3'){
             const survey = 'survey_rate_3';
-            await updateTallyPost(data[index].id,survey)
+            await updateTallyPost(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
         if(data[index].rate == '4'){
             const survey = 'survey_rate_4';
-            await updateTallyPost(data[index].id,survey)
+            await updateTallyPost(data[index].id,survey).then((c)=>{
+                console.log("end")
+            })
         }
     }
-    (await conn()).destroy();
+    await conn.end
     return res.json({code:200,message:"Accepted!"})
 })
 
 app.post("/api/percentage",async (req,res)=>{
-    level_one_percentage = (await conn()).query(`SELECT * FROM tbl_lvl_one WHERE acc_id = ${req.body.id}`)
-    level_two_percentage = (await conn()).query(`SELECT * FROM tbl_lvl_two WHERE acc_id = ${req.body.id}`)
+    level_one_percentage = (await conn()).query(`SELECT * FROM tbl_lvl_one WHERE acc_id = ${req.body.id}`).then((c)=>{
+        return c
+    })
+    level_two_percentage = (await conn()).query(`SELECT * FROM tbl_lvl_two WHERE acc_id = ${req.body.id}`).then((c)=>{
+        return c
+    })
 
-    skills = (await conn()).query(`SELECT * FROM tbl_career_skills WHERE acc_id = ${req.body.id}`)
-    interest = (await conn()).query(`SELECT * FROM tbl_career_interest WHERE acc_id = ${req.body.id}`)
-    attitude = (await conn()).query(`SELECT * FROM tbl_career_attitude WHERE acc_id = ${req.body.id}`)
+    skills = (await conn()).query(`SELECT * FROM tbl_career_skills WHERE acc_id = ${req.body.id}`).then((c)=>{
+        return c
+    })
+    interest = (await conn()).query(`SELECT * FROM tbl_career_interest WHERE acc_id = ${req.body.id}`).then((c)=>{
+        return c
+    })
+    attitude = (await conn()).query(`SELECT * FROM tbl_career_attitude WHERE acc_id = ${req.body.id}`).then((c)=>{
+        return c
+    })
 
     await level_one_percentage
     await level_two_percentage
     await skills
     await interest
     await attitude
-    (await conn()).destroy();
+    await conn.end
     return res.json({LevelOne:level_one_percentage,LevelTwo:level_two_percentage,skills:skills,interest:interest,attitude:attitude})
 })
 
 app.post("/api/level/three/save",async(req,res)=>{
-    find = (await conn()).query(`SELECT * FROM tbl_lvl_three WHERE acc_id = ${req.body.id}`)
-    length = await find
-    if(length.length > 0){
-        update = (await conn()).query(`UPDATE tbl_lvl_three SET mode = '${req.body.fields}', score = '${req.body.score}' 
-            WHERE acc_id = '${req.body.id}'`)
-    }else{
-    save = (await conn()).query(`INSERT INTO tbl_lvl_three (id,acc_id,mode,score) 
-        VALUES(null,'${req.body.id}','${req.body.fields}','${req.body.score}')`);
-    }
-    (await conn()).destroy();
+    find = (await conn()).query(`SELECT * FROM tbl_lvl_three WHERE acc_id = ${req.body.id}`).then(async (c)=>{
+        if(c.length > 0){
+            update = (await conn()).query(`UPDATE tbl_lvl_three SET mode = '${req.body.fields}', score = '${req.body.score}' 
+                WHERE acc_id = '${req.body.id}'`).then(()=>{
+                    console.log("end")
+                })
+            }else{
+                save = (await conn()).query(`INSERT INTO tbl_lvl_three (id,acc_id,mode,score) 
+                    VALUES(null,'${req.body.id}','${req.body.fields}','${req.body.score}')`).then(()=>{
+                        console.log("end")
+                    })
+                }
+            })
+    await conn.end
     return res.json({code:200,message:"Accepted!"})
 })
 
