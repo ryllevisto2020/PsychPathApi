bcba12a47cea3590e705a3b3208f8cee03e52592
root <root@srv1134934.hstgr.cloud>
2025-11-18 08:30:01 +0000

    server

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.post("/api/registration",async (req,res)=>{
     addAccount = (await conn()).query(
         `INSERT INTO tbl_account (id,acc_username,acc_password,acc_name,acc_age,acc_section,acc_gender) `+
         `VALUES(null,'${req.body.username}','${req.body.password}','${req.body.name}','${req.body.age}','${req.body.section}','${req.body.gender}')`);
+    
+    (await conn()).destroy();
 
     return res.json({code:200,message:"Accepted"})
 })
@@ -48,8 +50,10 @@ app.post("/api/login",async (req,res)=>{
     account = await loginAccount
 
     if(account.length > 0 ){
+        (await conn()).destroy();
         return res.json({code:200,message:"Accepted!",id:account[0].id});
     }else{
+        (await conn()).destroy();
         return res.json({code:500,message:"Rejected!"});
     }
 })
@@ -67,6 +71,7 @@ app.post("/api/level/one/save",async (req,res)=>{
         save = (await conn()).query(`INSERT INTO tbl_lvl_one (id,acc_id,res_clinical,res_industrial,res_educational	) 
         VALUES(null,'${req.body.id}','${Math.round((req.body.count.A/18) * 50)}','${Math.round((req.body.count.B/18) * 50)}','${Math.round((req.body.count.C/18) * 50)}')`);
     }
+    (await conn()).destroy();
     return res.json({code:200,message:"Accepted!"})
 })
 
@@ -81,6 +86,7 @@ app.post("/api/level/two/save",async(req,res)=>{
         save = (await conn()).query(`INSERT INTO tbl_lvl_two (id,acc_id,res_clinical,res_educational,res_industrial) 
         VALUES(null,'${req.body.id}','${Math.round((req.body.Clinical/5) * 50)}','${Math.round((req.body.Educational/5) * 50)}','${Math.round((req.body.Industrial/5) * 50)}')`)
     }
+	(await conn()).destroy();
     return res.json({code:200,message:"Accepted!"})
 })
 
@@ -150,7 +156,7 @@ app.post("/api/survey/pre",async (req,res)=>{
         save_skills = (await conn()).query(`INSERT INTO tbl_career_skills (id,acc_id,score) 
         VALUES(null,'${req.body.id}','${Math.round(percentage_set_C/6)}')`)
     }
-    
+   (await conn()).destroy(); 
 
     return res.json({A:percentage_set_A/4,B:percentage_set_B/5,C:percentage_set_C/6})
 })
@@ -176,6 +182,7 @@ app.post("/api/survey/post",async(req,res)=>{
             await updateTallyPost(data[index].id,survey)
         }
     }
+    (await conn()).destroy();
     return res.json({code:200,message:"Accepted!"})
 })
 
@@ -192,12 +199,12 @@ app.post("/api/percentage",async (req,res)=>{
     await skills
     await interest
     await attitude
-
+    (await conn()).destroy();
     return res.json({LevelOne:level_one_percentage,LevelTwo:level_two_percentage,skills:skills,interest:interest,attitude:attitude})
 })
 
 app.post("/api/level/three/save",async(req,res)=>{
-    find = (await conn()).query(`SELECT * FROM tbl_lvl_two WHERE acc_id = ${req.body.id}`)
+    find = (await conn()).query(`SELECT * FROM tbl_lvl_three WHERE acc_id = ${req.body.id}`)
     length = await find
     if(length.length > 0){
         update = (await conn()).query(`UPDATE tbl_lvl_three SET mode = '${req.body.fields}', score = '${req.body.score}' 
@@ -206,7 +213,7 @@ app.post("/api/level/three/save",async(req,res)=>{
     save = (await conn()).query(`INSERT INTO tbl_lvl_three (id,acc_id,mode,score) 
         VALUES(null,'${req.body.id}','${req.body.fields}','${req.body.score}')`);
     }
-
+    (await conn()).destroy();
     return res.json({code:200,message:"Accepted!"})
 })
 
@@ -219,6 +226,7 @@ app.post("/api/leaderboard",async(req,res)=>{
     await clinical
     await educational
     await industrial
+   (await conn()).destroy();
     return res.json({overall:overall,clinical:clinical,educational:educational,industrial:industrial})
 })
 
